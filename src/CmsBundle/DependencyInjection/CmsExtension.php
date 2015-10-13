e9e498fdf4fa0ae060d6a325c6c4972b382d0f01
Morior <moriorgames@gmail.com>
2015-10-13 12:23:50 +0200

    Solve some critical issues

--- src/CmsBundle/DependencyInjection/CmsExtension.php
+++ src/CmsBundle/DependencyInjection/CmsExtension.php
@@ -22,7 +22,7 @@ class CmsExtension extends Extension
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
 }
